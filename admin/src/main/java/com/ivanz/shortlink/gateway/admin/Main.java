d707efbec44f0e34b20838de2efd7227d3111e95
zhangyifan69 <zhangyifan69@meituan.com>
2024-05-23 17:30:05 +0800

    创建子模块

---
+++ admin/src/main/java/com/ivanz/shortlink/gateway/admin/Main.java
@@ -0,0 +1,8 @@
+package com.ivanz.shortlink.gateway.admin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ gateway/src/main/java/com/ivanz/shortlink/gateway/Main.java
@@ -0,0 +1,8 @@
+package com.ivanz.shortlink.gateway;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
